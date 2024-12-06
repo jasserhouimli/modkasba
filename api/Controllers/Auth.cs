70782db092b01ac369f1dbe043cd8a0790e78305
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2024-12-06 14:03:40 +0100

    d

--- api/Controllers/Auth.cs
+++ api/Controllers/Auth.cs
@@ -14,7 +14,7 @@ namespace api.Controllers
         [HttpPost]
         public IActionResult Login()
         {
-            return Ok("Hello Auth Jasser");
+            return Ok("Hello AUth uss");
         }
     }
 }
