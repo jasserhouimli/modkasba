3aea0f6baf2426375dfa0af2e215d9ded0ec1728
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2024-12-06 13:34:28 +0100

    Update .gitignore to exclude bin and obj directories

--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,6 +15,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.MapControllers();
 
 app.Run();
 
