e6dc963e77e38ce461abe79ed116ab5ffbe55b64
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2024-12-06 12:08:13 +0100

    from test branch

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// OpenAPI is a standard way to document RESTful APIs.
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -21,7 +21,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
