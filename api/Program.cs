529549a63de7e1e42a9c95bc2c5753cd57e0b9f8
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2024-12-06 15:32:04 +0100

    fix

--- api/Program.cs
+++ api/Program.cs
@@ -1,16 +1,27 @@
-
-
-
 using Microsoft.EntityFrameworkCore;
 using api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Replace with your React app's URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=app.db"));
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -21,8 +32,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Enable CORS
+app.UseCors("AllowReactApp");
 
 app.MapControllers();
-
 app.Run();
-
