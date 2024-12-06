aa3e5a52735e3d134708f78177f5c222df5445a9
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2024-12-06 14:30:53 +0100

    dd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
-export default async function Home() {
-  const response = await fetch("http://localhost:5084/api/User");
-  const data = await response.json();
-  return <h1>Hello Swag {data.message}</h1>;
+export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <form
+        action=""
+        className="flex flex-col gap-4 border border-1 p-4 rounded-md"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
